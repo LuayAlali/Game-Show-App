08fd38394efa489bd60796d30727adcfc9cb1d64
LuayAlali <Luayalali@gmail.com>
2020-07-19 13:30:56 -0700

    updated file(s) location, & added eventListener on startButton
    created a start button variable called resetButton associated with the ID btn__reset in HTML.

---
+++ app.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const resetButton = document.querySelector('.btn__reset');
+let missedGuesses = 0;
+const overlay = document.getElementById('overlay');
+const phraseArray = ['books',
+					 'fan',
+					 'hat',
+					 'computer',
+					 'bicycle'
+					];
+
+resetButton.addEventListener('click', () => {
+	overlay.style.display = 'none';
+	
+});
+
