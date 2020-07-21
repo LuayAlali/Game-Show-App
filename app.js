a2a89159e32b0fb7d64ffa4225e9b1522c188bbe
LuayAlali <Luayalali@gmail.com>
2020-07-20 19:06:48 -0700

    added a click event listenere for the buttons

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const resetButton = document.querySelector('.btn__reset');
 let missedGuesses = 0;
 const overlay = document.getElementById('overlay');
 const phraseArray = ['books',
-					 'fan',
-					 'hat',
+					 'fans',
+					 'hats',
 					 'computer',
 					 'bicycle'
 					];
 const splitCharacters = getRandomPhraseAsArray(phraseArray);
-
+const buttons = document.getElementsByTagName('button');
 
 resetButton.addEventListener('click', () => {
 	overlay.style.display = 'none';
@@ -29,10 +29,10 @@ function getRandomPhraseAsArray(phrases) {
 
 
 function addPhraseToDisplay(character){
+	const ul = document.querySelector('#phrase');
 	for(let i = 0; i < character.length; i++){
 		let li = document.createElement('li');
 		li.textContent = `${character[i]}`;
-		const ul = document.querySelector('#phrase');
 		 ul.appendChild(li);
 		if( character[i] != -1 ){
 			 li.className = 'letter';
@@ -45,16 +45,27 @@ function addPhraseToDisplay(character){
 }
 function checkLetter(clicked) {
 	let letters = document.querySelectorAll('.letter')
+	let match = null;
 	for(let i = 0; i <  letters.length; i++){
 		if(clicked === letters[i]){
 		letters[i].className = 'show';
+			match.textContent += letters[i].textContent;
+			return match;
 	} else 
 		return null;
 		
 		}
 }
 
-
+qwerty.addEventListener('click', (event) => {
+	if (event.target.tagName != 'BUTTON' || event.target.className === 'chosen') {
+		return null
+		}  else if (event.target.tagName == 'BUTTON') {
+	   event.target.className = 'chosen';
+		let letterFound = checkLetter(event);
+		return letterFound;
+	   }
+});
 
 
 addPhraseToDisplay(splitCharacters);
